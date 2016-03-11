9a9a64752adc656c8166e68c937f1ea12104b125
shawnbutts <me@shawnbutts.com>
2016-03-11 08:50:41 -0500

    0.0.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open('dev-requirements.txt') as f:
 
 setup(
     name='netuitive_statsd',
-    version='0.0.1',
+    version='0.0.2',
     description="Netuitive StatsD server",
     long_description='Netuitive StatsD server\n',
     author="Netuitive",
