d2ed0e247bfdce6f3a122b47181cab6c7c2a2ebe
shawnbutts <shawnbutts@users.noreply.github.com>
2016-09-30 18:37:36 -0400

    fix millisecond timestamps

--- libs/statsd/util.py
+++ libs/statsd/util.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 def to_ms_timestamp(dt, epoch=datetime(1970, 1, 1)):
     td = dt - epoch
-    return (td.microseconds + (td.seconds + td.days * 86400) * 10**3)
+    return ((td.microseconds + (td.seconds + td.days * 86400) * 10**6) / 10**3)
 
 
 def get_timestamp(dt=None, epoch=datetime(1970, 1, 1)):
